6b4d66472e088c7d725bdd85a8eb9a859facd785
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-10-17 18:31:59 +0600

    Navbar Created

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { createBrowserRouter } from "react-router";
+import Root from "../pages/Root/Root";
+import Error from "../pages/ErrorPage/Error";
+import Home from "../pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement: <Error></Error>,
+    children: [
+        {
+            index: true,
+            path: "/",
+            Component: Home
+        }
+    ]
+  },
+]);
