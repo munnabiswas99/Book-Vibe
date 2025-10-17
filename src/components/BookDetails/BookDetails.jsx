5706e63ae95ce2e3e710575252b6c048095896ee
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-10-18 00:32:23 +0600

    Book details added

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -9,13 +9,29 @@ const BookDetails = () => {
     // console.log(data);
 
     const singleBook = data.find(book => book.bookId === bookId);
-    const {bookName, image} = singleBook;
+    const {bookName, image, author, category, review, totalPages, publisher, yearOfPublishing, rating} = singleBook;
     return (
-        <div>
-            <h4>Book Details</h4>
-            <div>
-                <img src={image} alt="" />
-                <h3>{bookName}</h3>
+        <div className='m-10 p-5 '>
+            <div className='flex gap-10'>
+                <img className='h-[400px] rounded-sm p-10 bg-gray-100' src={image} alt="" />
+                <div>
+                    <h3 className='font-semibold text-2xl'>{bookName}</h3>
+                    <p>By: {author}</p>
+                    <div className='border my-1'></div>
+                    <p>{category}</p>
+
+                    <p><span className='font-bold'>Review:</span>{review}</p>
+                    <p><span className='font-bold'>Tag </span></p>
+                    <div className='border my-1'></div>
+
+                    <p>Number of Pages: <span className='font-bold'>{totalPages}</span></p>
+                    <p>Publisher: <span className='font-bold'>{publisher}</span></p>
+                    <p>Year of Publishing: <span className='font-bold'>{yearOfPublishing}</span></p>
+                    <p>Rating: <span className='font-bold'>{rating}</span></p>
+
+                    <button className="btn btn-accent m-2">Read</button>
+                    <button className="btn btn-info m2">WishList</button>
+                </div>
             </div>
         </div>
     );
