61efffe7662343a53e4d75c194895571d9af7bf3
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-10-18 22:03:24 +0600

    Sweet Alert Aded

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,45 +1,90 @@
-import React from 'react';
-import { useLoaderData, useParams } from 'react-router';
-import { addToStoredDB } from '../../utility/addToDB';
+import React from "react";
+import { useLoaderData, useParams } from "react-router";
+import { addToStoredDB } from "../../utility/addToDB";
+
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
 
 const BookDetails = () => {
-    const {id} = useParams();
-    const bookId = parseInt(id);
-    // console.log(id);
-    const data = useLoaderData();
-    // console.log(data);
+  const { id } = useParams();
+  const bookId = parseInt(id);
+  // console.log(id);
+  const data = useLoaderData();
+  // console.log(data);
 
-    const singleBook = data.find(book => book.bookId === bookId);
-    const {bookName, image, author, category, review, totalPages, publisher, yearOfPublishing, rating} = singleBook;
+  const singleBook = data.find((book) => book.bookId === bookId);
+  const {
+    bookName,
+    image,
+    author,
+    category,
+    review,
+    totalPages,
+    publisher,
+    yearOfPublishing,
+    rating,
+  } = singleBook;
 
-    const handleMarkAsRead = (id) => {
-        addToStoredDB(id);
-    }
-    return (
-        <div className='m-10 p-5 '>
-            <div className='flex gap-10'>
-                <img className='h-[400px] rounded-sm p-10 bg-gray-100' src={image} alt="" />
-                <div>
-                    <h3 className='font-semibold text-2xl'>{bookName}</h3>
-                    <p>By: {author}</p>
-                    <div className='border my-1'></div>
-                    <p>{category}</p>
+  const handleMarkAsRead = (id) => {
+    MySwal.fire({
+  title: "Good job!",
+  text: "Book Added to Read List",
+  icon: "success"
+});
 
-                    <p><span className='font-bold'>Review:</span>{review}</p>
-                    <p><span className='font-bold'>Tag </span></p>
-                    <div className='border my-1'></div>
+    addToStoredDB(id);
+  };
 
-                    <p>Number of Pages: <span className='font-bold'>{totalPages}</span></p>
-                    <p>Publisher: <span className='font-bold'>{publisher}</span></p>
-                    <p>Year of Publishing: <span className='font-bold'>{yearOfPublishing}</span></p>
-                    <p>Rating: <span className='font-bold'>{rating}</span></p>
+  return (
+    <div className="m-10 p-5 ">
+      <div className="flex gap-10">
+        <img
+          className="h-[400px] rounded-sm p-10 bg-gray-100"
+          src={image}
+          alt=""
+        />
+        <div>
+          <h3 className="font-semibold text-2xl">{bookName}</h3>
+          <p>By: {author}</p>
+          <div className="border my-1"></div>
+          <p>{category}</p>
 
-                    <button onClick={()=>handleMarkAsRead(id)} className="btn btn-accent m-2">Mark As Read</button>
-                    <button className="btn btn-info m2">Add to WishList</button>
-                </div>
-            </div>
+          <p>
+            <span className="font-bold">Review:</span>
+            {review}
+          </p>
+          <p>
+            <span className="font-bold">Tag </span>
+          </p>
+          <div className="border my-1"></div>
+
+          <p>
+            Number of Pages: <span className="font-bold">{totalPages}</span>
+          </p>
+          <p>
+            Publisher: <span className="font-bold">{publisher}</span>
+          </p>
+          <p>
+            Year of Publishing:{" "}
+            <span className="font-bold">{yearOfPublishing}</span>
+          </p>
+          <p>
+            Rating: <span className="font-bold">{rating}</span>
+          </p>
+
+          <button
+            onClick={() => handleMarkAsRead(id)}
+            className="btn btn-accent m-2"
+          >
+            Mark As Read
+          </button>
+          <button className="btn btn-info m2">Add to WishList</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default BookDetails;
--- src/pages/ReadList/ReadList.jsx
+++ src/pages/ReadList/ReadList.jsx
@@ -8,6 +8,7 @@ import Book from '../Book/Book';
 const ReadList = () => {
 
     const [readList, setReadList] = useState([]);
+    const [sort, setSort] = useState("");
 
     const data = useLoaderData();
     console.log(data);
@@ -20,8 +21,29 @@ const ReadList = () => {
         setReadList(myReadList);
     },[])
 
+    const handleSort = (type) => {
+        setSort(type);
+        if (type === "pages") {
+            // Ascending order
+            const sortedByPage = [...readList].sort((a,b) => a.totalPages - b.totalPages);
+            setReadList(sortedByPage);
+        } 
+        if (type === "rating") {
+            // Descending order
+            const sortedByRating = [...readList].sort((a,b) => b.rating - a.rating);
+            setReadList(sortedByRating);
+        } 
+    }
+
     return (
         <div>
+            <details className="dropdown">
+                <summary className="btn m-1">Short By: {sort?sort:""}</summary>
+                    <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <li><a onClick={()=>handleSort("pages")}>Number of Pages</a></li>
+                        <li><a onClick={()=>handleSort("rating")}>Rating</a></li>
+                    </ul>
+                </details>
             <Tabs>
                 <TabList>
                 <Tab>Read List</Tab>
